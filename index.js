5d26e25adca4d18bfd3041bd69ffe44470344f4d
kanaka prasad <kanakaprasad1815@gmail.com>
2026-01-04 12:06:04 +0530

    feat: Configure environment variables and establish MongoDB connection.

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
+require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+const mongoUri = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/todo-app';
+
+mongoose.connect(mongoUri)
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('Could not connect to MongoDB:', err));
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
